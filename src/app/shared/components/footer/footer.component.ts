11877bf5feb9cc6bc36b54b8780a752f6d6b18fd
Alex2 <aleksander1802@gmail.com>
2024-01-26 16:29:48 +0300

    feat: added footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+    feedbackForm!: FormGroup;
 
+    navLinks = [
+        { title: 'О нас', link: '/' },
+        { title: 'Все объекты', link: '/objects' },
+        { title: 'Продажа', link: '/sell' },
+        { title: 'Аренда', link: '/rent' },
+        { title: 'Купить, продать, арендовать с нами', link: '/buy-sell-rent' },
+        { title: 'Презентация', link: '/presentation' },
+        { title: 'Экспертная оценка', link: '/expertise' },
+        { title: 'Управление недвижимостью', link: '/property' },
+        { title: 'Консьерж-сервис', link: '/concierge' },
+        { title: 'Наша комиссия', link: '/fee' },
+        { title: 'Политика конфиденциальности', link: '/policy' },
+    ];
+
+    regions = [
+        { title: 'Апартаменты в Больё-сюр-Мер' },
+        { title: 'Вилла в Больё-сюр-Мер' },
+        { title: 'Вилла в Эзе' },
+        { title: 'Вилла в Сен-Жан-Кап-Ферра' },
+        { title: 'Апартаменты в Эзе' },
+        { title: 'Апартаменты в Сен-Жан-Кап-Ферра' },
+    ];
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    private initForm() {
+        this.feedbackForm = this.fb.group({
+            name: ['', [Validators.required]],
+            phone: ['', [Validators.required]],
+            email: ['', [Validators.required, Validators.email]],
+        });
+    }
+
+    get name() {
+        return this.feedbackForm.get('name');
+    }
+
+    get email() {
+        return this.feedbackForm.get('email');
+    }
+
+    get phone() {
+        return this.feedbackForm.get('phone');
+    }
+
+    getError(controlName: string, errorName: string) {
+        return this.feedbackForm.get(controlName)?.hasError(errorName);
+    }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,17 @@ import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [MainLayoutComponent, HeaderComponent, MenuComponent],
-    imports: [CommonModule, RouterModule],
+    declarations: [
+        MainLayoutComponent,
+        HeaderComponent,
+        MenuComponent,
+        FooterComponent,
+    ],
+    imports: [CommonModule, RouterModule, ReactiveFormsModule],
     exports: [HeaderComponent, MenuComponent],
 })
 export class SharedModule {}
