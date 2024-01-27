30d1c36be3d44829a9841c29c033ca000de9fbe5
Alex2 <aleksander1802@gmail.com>
2024-01-27 18:35:20 +0300

    feat: added fourth screen

---
+++ src/app/pages/1-home/components/home-fourth-screen/home-fourth-screen.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home-fourth-screen',
+    templateUrl: './home-fourth-screen.component.html',
+    styleUrl: './home-fourth-screen.component.scss',
+})
+export class HomeFourthScreenComponent {}
--- src/app/pages/1-home/home.module.ts
+++ src/app/pages/1-home/home.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home.component';
 import { HomeSecondScreenComponent } from './components/2-home-second-screen/home-second-screen.component';
 import { HomeThirdScreenComponent } from './components/3-home-third-screen/home-third-screen.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HomeFourthScreenComponent } from './components/home-fourth-screen/home-fourth-screen.component';
 
 @NgModule({
     declarations: [
@@ -12,8 +13,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
         HomeFirstComponent,
         HomeSecondScreenComponent,
         HomeThirdScreenComponent,
+        HomeFourthScreenComponent,
     ],
     imports: [CommonModule, SharedModule],
-    
 })
 export class HomeModule {}
